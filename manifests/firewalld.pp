d0a683a361e6808e2f0432860a614d699af88a4d
Brian Flannery <bflannery@caci.com>
2016-07-01 10:44:06 -0600

    d20160701T164406z: Repairs to manifest files for Puppet Lint

--- manifests/firewalld.pp
+++ manifests/firewalld.pp
@@ -7,10 +7,10 @@ class jenkins::firewalld {
   if $caller_module_name != $module_name {
     fail("Use of private class name ${name} by caller_module_name ${caller_module_name}, whereas module_name is ${module_name}.")
   }
-  
+
   # if $::firewalld {
   if defined('::firewalld') {
-    
+
     firewalld_port { 'Open port in the public zone for HTTP Jenkins web GUI':
       ensure   => present,
       zone     => 'public',
@@ -21,15 +21,15 @@ class jenkins::firewalld {
       firewalld_port { 'Open port in the public zone for Jenkins JNLP slaves':
         ensure   => present,
         zone     => 'public',
-        port     => "${::jenkins::slaveagentport}",
+        port     => $::jenkins::slaveagentport,
         protocol => 'tcp',
       }
     }
-    
-    
+
+
   } else {
     fail("The firewalld class expects to see a defined '::firewalld'.")
 
   }
-  
+
 }
