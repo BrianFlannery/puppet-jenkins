76ba0922f72264f54543779373e63076befe8f61
Brian Flannery <bflannery@caci.com>
2016-07-07 15:33:39 -0600

    d20160707T21z: See if spec fails where parallel_spec does not

--- not_puppet/test.sh
+++ not_puppet/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-[[ $SKIP_BEAKER ]]   || SKIP_BEAKER=false ;
+[[ $SKIP_BEAKER ]]   || SKIP_BEAKER=true ;
 [[ $BEAKER_UBUNTU ]] || BEAKER_UBUNTU=false ;
 
 main() {
   cd .. && {
-    # execute bundle exec rake lint
+    execute bundle exec rake lint
     # execute bundle exec rake parallel_spec SPEC_OPTS='--format documentation --order random'
-    # export BEAKER_provision=yes ;
+    execute bundle exec rake spec SPEC_OPTS='--format documentation --order random'
+    export BEAKER_provision=yes ;
     export BEAKER_set="centos-7-docker" ;
     execute bundle exec rake acceptance ;
     if [[ false == $BEAKER_UBUNTU ]] ; then
