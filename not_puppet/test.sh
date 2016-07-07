def86e56dd189c8c3f8bfaaceabce73bf8ad3a38
Brian Flannery <bflannery@caci.com>
2016-07-07 13:52:01 -0600

    d20160707T19z: Include a script (not_puppet/test.sh) for local testing to match Travis CI.

---
+++ not_puppet/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+[[ $SKIP_BEAKER ]]   || SKIP_BEAKER=false ;
+[[ $BEAKER_UBUNTU ]] || BEAKER_UBUNTU=false ;
+
+main() {
+  cd .. && {
+    # execute bundle exec rake lint
+    # execute bundle exec rake parallel_spec SPEC_OPTS='--format documentation --order random'
+    # export BEAKER_provision=yes ;
+    export BEAKER_set="centos-7-docker" ;
+    execute bundle exec rake acceptance ;
+    if [[ false == $BEAKER_UBUNTU ]] ; then
+      export BEAKER_set="ubuntu-14.04-docker" ;
+      execute bundle exec rake acceptance ;
+    fi ;
+  }
+}
+execute() {
+  local err='' ;
+  "$@" || err=$? ;
+  [[ $err -lt 1 ]] || { echo "ERROR '$err' with {$@}." ; exit $err ; }
+}
+
+cd $(dirname $0) && main ;
+
+#
