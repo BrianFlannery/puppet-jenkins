b10985f735cc0b36cbf9b72cff75cb9c7fbc3560
Brian Flannery <bflannery@caci.com>
2016-07-08 10:12:36 -0600

    d20160708T16z: Add jhoblitt as a usual Git upstream

--- not_puppet/git.sh
+++ not_puppet/git.sh
@@ -3,12 +3,20 @@
 upstreamGitUrl='git@github.com:jenkinsci/puppet-jenkins' ;
 upstreamGitUrlRegExp='github[.]com.jenkinsci/puppet[\-]jenkins' ;
 upstreamRemoteName='upstream' ;
+upstream2() {
+  local upstreamGitUrl='git@github.com:jhoblitt/puppet-jenkins' ;
+  local upstreamGitUrlRegExp='github[.]com.jhoblitt/puppet[\-]jenkins' ;
+  local upstreamRemoteName='upstream2' ;
+  local hasRemote=`git remote -v | egrep "$upstreamGitUrlRegExp"` ;
+  [[ $hasRemote ]] || execute git remote add "$upstreamRemoteName" "$upstreamGitUrl" ;
+}
 
 main() {
   local hasRemote='' ;
   cd .. && {
     hasRemote=`git remote -v | egrep "$upstreamGitUrlRegExp"` ;
     [[ $hasRemote ]] || execute git remote add "$upstreamRemoteName" "$upstreamGitUrl" ;
+    upstream2 ;
   }
 }
 execute() {
